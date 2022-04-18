066ca6d4017f01c13291976a2d996eaf595b8754
Nastia Zaitseva <nastiazaitseva@MacBook-Pro-Nastia.local>
2022-04-18 19:46:28 +0300

    Initial commit

---
+++ src/com/company/HwEight.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class HwEight {
+
+    public static void main(String[] args) {
+
+        int a = 100;
+        int b = 10;
+        if (a % 2 == 0) {
+            System.out.println("'a' even");
+        } else {
+            System.out.println("'a' odd");}
+        if (b % 2 == 0) {
+            System.out.println("'b' even");
+        } else {
+            System.out.println("'b' odd");}
+    }
+}
+
---
+++ src/com/company/HwSeven.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class HwSeven {
+
+
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 20;
+
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
